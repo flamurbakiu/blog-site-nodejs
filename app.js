baa966763d5c1504f2f19d0fa27b914a02222ccd
bakiuflamur <flamur.b@appgr8.com>
2022-03-01 12:25:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const morgan = require('morgan');
+
+// express app
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+app.listen(3000);
+
+// middleware & static-files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+// listen for requests
+app.get('/', (req, res) => {
+  const blogs = [
+    {
+      title: 'Yoshi finds eggs',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'Mario finds stars',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'How to defeat bowser',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a Blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
